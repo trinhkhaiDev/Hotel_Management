2033bc0dd0378f9e4e6a961f54eea365e9dc8626
trinhkhai <khaitv21@uef.edu.vn>
2023-10-08 16:31:08 +0700

    init project

---
+++ Hotel_Management/Form1.cs
@@ -0,0 +1,15 @@
+namespace Hotel_Management
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbTitle.Text = "Hello World!";
+        }
+    }
+}
